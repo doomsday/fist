89f37ce1e5e0929e125835352a7dd6ef1d2d1e97
Mikhail Gashenko <dr.psycho@live.com>
2017-08-31 01:07:34 +0600

    1) Writing the first routes 2) Restricting the route parameters 3) Performing redirections 4) Returning views

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,20 @@
 //    return view('welcome');
 //});
 
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function() {
+    return redirect('cats');
+});
+
+Route::get('cats', function() {
     return 'All cats';
 });
 
 Route::get('cats/{id}', function($id) {
     return sprintf('Cat #%s', $id);
+})->where('id', '[0-9]+');
+
+Route::get('about', function(){
+    return view('about')->with('number_of_cats', 9000);
 });
