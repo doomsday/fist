37cfd344e804cdb41ca1be67c7e95bbbc28dddb5
Mikhail Gashenko <dr.psycho@live.com>
2017-09-07 22:54:18 +0600

    The overview page

--- routes/web.php
+++ routes/web.php
@@ -22,12 +22,18 @@ Route::get('/', function() {
 });
 
 Route::get('cats', function() {
-    return 'All cats';
+    $cats = Furbook\Cat::all();
+    return view('cats.index')->with('cats', $cats);
 });
 
 Route::get('cats/{id}', function($id) {
     return sprintf('Cat #%s', $id);
 })->where('id', '[0-9]+');
+
+Route::get('cats/breeds/{name}', function($name) {
+   $breed = Furbook\Breed::with('cats')->whereName($name)->first();
+   return view('cats.index')->with('breed', $breed)->with('cats', $breed->cats);
+});
 
 Route::get('about', function(){
     return view('about')->with('number_of_cats', 9000);
